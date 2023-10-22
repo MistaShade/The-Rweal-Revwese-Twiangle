440f51ac6f536f91afa326daf804242d6a69d106
Sam <114481646+MistaShade@users.noreply.github.com>
2023-10-22 16:29:50 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, x, y;
+            Console.Write("Enter the height of the triangle: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (y = 1; y <= num; y++)
+            {
+                for (x = 1; x <= y; x++)
+                {
+                    Console.Write("* ");
+                }
+                for (x = y + 1; x <= num; x++)
+                {
+                    Console.Write("_ ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+    }
+}
